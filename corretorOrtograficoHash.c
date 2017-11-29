d2c856f11da7382749f43b3eb62f093c5d8a229c
joaovitorml <joao.lemos4000@gmail.com>
2017-11-29 20:31:36 -0200

    Novo Avanço

--- corretorOrtograficoHash.c
+++ corretorOrtograficoHash.c
@@ -28,42 +28,14 @@
 unsigned long long int hashtable[MAX];
 int top=0;
 
-typedef union _Info
-{
-	unsigned long value;
-	unsigned char bytes[4];
-} Info;
-
-unsigned long
-hash(const char *text)
-{
-	int size, i, h;
-	Info info;
-
-	size = strlen(text);
-	h = 0;
-
-	for (i = 0, info.value = 0; i < 4
-		&& i < size; i++)
-	{
-		info.bytes[i] = text[i];
-	}
-
-	h = info.value;
-
-	for (i = 4, info.value = 0; i < size; i++)
-	{
-		info.bytes[i % 4] = text[i];
-
-		if (i % 4 == 3)
-		{
-			h ^= info.value;
-			info.value = 0;
+int hash(const char *palavra){
+		int i,h = palavra[0];
+		for(i=1;palavra[i]!='\0';i++){
+			h = (h * 251 + palavra[i])%5000000;
 		}
+		return h;
 	}
 
-	return h;
-}
 
 /* Retorna true se a palavra estah no dicionario. Do contrario, retorna false */
 bool conferePalavra(const char *palavra) {
