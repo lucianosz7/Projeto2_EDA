13838db9da5ebbcb01e225f62836cc5e056822b1
João Lucas Sousa Reis <joao.lucas.ssr@gmail.com>
2017-11-29 21:15:01 -0300

    aaaaaaaa

--- corretorOrtograficoArvore.c
+++ corretorOrtograficoArvore.c
@@ -28,18 +28,17 @@ int top=0;
 #define ARQTEXTO_ERROLEITURA    4
 #define ERRO_DICIO_NAOCARREGADO 5
 
-struct Node
-{
+struct Node{
     char palavra[TAM_MAX];
     struct Node *left;
     struct Node *right;
     int height;
 };
 
-// A utility function to get maximum of two integers
+// Uma função para obter o máximo de dois inteiros
 int max(int a, int b);
 
-// A utility function to get height of the tree
+// Função para obter a altura da arvore
 int height(struct Node *N)
 {
     if (N == NULL)
@@ -47,14 +46,13 @@ int height(struct Node *N)
     return N->height;
 }
 
-// A utility function to get maximum of two integers
+// Uma função para obter o máximo de dois inteiros
 int max(int a, int b)
 {
     return (a > b)? a : b;
 }
 
-/* Helper function that allocates a new node with the given key and
-    NULL left and right pointers. */
+// Função que aloca um novo nó com a chave dada
 struct Node* newNode(char *palavra)
 {
     struct Node* node = (struct Node*)
@@ -62,49 +60,41 @@ struct Node* newNode(char *palavra)
     strcpy(node->palavra,palavra);
     node->left   = NULL;
     node->right  = NULL;
-    node->height = 1;  // new node is initially added at leaf
+    node->height = 1;
     return(node);
 }
 
-// A utility function to right rotate subtree rooted with y
-// See the diagram given above.
+// função para girar corretamente a subárvore da raiz y
 struct Node *rightRotate(struct Node *y)
 {
     struct Node *x = y->left;
     struct Node *T2 = x->right;
 
-    // Perform rotation
     x->right = y;
     y->left = T2;
 
-    // Update heights
     y->height = max(height(y->left), height(y->right))+1;
     x->height = max(height(x->left), height(x->right))+1;
 
-    // Return new root
     return x;
 }
 
-// A utility function to left rotate subtree rooted with x
-// See the diagram given above.
+// função para girar corretamente a subárvore da raiz x
 struct Node *leftRotate(struct Node *x)
 {
     struct Node *y = x->right;
     struct Node *T2 = y->left;
 
-    // Perform rotation
     y->left = x;
     x->right = T2;
 
-    //  Update heights
     x->height = max(height(x->left), height(x->right))+1;
     y->height = max(height(y->left), height(y->right))+1;
 
-    // Return new root
     return y;
 }
 
-// Get Balance factor of node N
+// Fator de equilíbrio do nó N
 int getBalance(struct Node *N)
 {
     if (N == NULL)
@@ -112,11 +102,9 @@ int getBalance(struct Node *N)
     return height(N->left) - height(N->right);
 }
 
-// Recursive function to insert key in subtree rooted
-// with node and returns new root of subtree.
-struct Node* insert(struct Node* node, char *palavra)
-{
-    /* 1.  Perform the normal BST insertion */
+// Função recursiva para inserir a chave na subárvore com nó e retornar uma nova raiz da subárvore.
+struct Node* insert(struct Node* node, char *palavra){
+
     if (node == NULL)
         return(newNode(palavra));
 
@@ -124,50 +112,39 @@ struct Node* insert(struct Node* node, char *palavra)
         node->left  = insert(node->left, palavra);
     else if (strcmp(palavra, node->palavra) > 0)
         node->right = insert(node->right, palavra);
-    else // Equal keys are not allowed in BST
+    else 
         return node;
 
-    /* 2. Update height of this ancestor node */
     node->height = 1 + max(height(node->left),
                            height(node->right));
 
-    /* 3. Get the balance factor of this ancestor
-          node to check whether this node became
-          unbalanced */
     int balance = getBalance(node);
 
-    // If this node becomes unbalanced, then
-    // there are 4 cases
-
-    // Left Left Case
     if (balance > 1 && strcmp(palavra, node->left->palavra) < 0)
         return rightRotate(node);
 
-    // Right Right Case
     if (balance < -1 && strcmp(palavra, node->right->palavra) > 0)
         return leftRotate(node);
 
-    // Left Right Case
     if (balance > 1 && strcmp(palavra, node->left->palavra) > 0)
     {
         node->left =  leftRotate(node->left);
         return rightRotate(node);
     }
 
-    // Right Left Case
     if (balance < -1 && strcmp(palavra, node->right->palavra) < 0)
     {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
 
-    /* return the (unchanged) node pointer */
     return node;
 }
 
 int tem_nada(struct Node *t){
   return t==NULL;
 }
+
 
 struct Node *motosserra(struct Node *no){
   if(!tem_nada(no)){
@@ -177,8 +154,6 @@ struct Node *motosserra(struct Node *no){
   }
   return NULL;
 }
-
-
 
 struct Node *arvore = NULL;
 
